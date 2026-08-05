8d626b27951d3284cf3bbe2133948f35883edde4
Kkkazhai <sergeykazhaev7@gmail.com>
2026-08-05 23:29:08 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,5 +1,21 @@
-from parsers.dummy import DummyParser
+from config import DATA_FOLDER
+from core.storage import Storage
+
+
+def main():
+    storage = Storage(DATA_FOLDER)
+
+    storage.save(
+        "status.json",
+        {
+            "project": "Lux Finder",
+            "version": "0.1.0",
+            "status": "running"
+        }
+    )
+
+    print("Lux Finder started")
+
 
 if __name__ == "__main__":
-    parser = DummyParser()
-    parser.run()
+    main()
