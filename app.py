227040058475d9fc6caa54590f63b24dfc05e54b
Kkkazhai <sergeykazhaev7@gmail.com>
2026-08-05 14:07:24 +0300

    Update app.py

--- app.py
+++ app.py
@@ -1,1 +1,9 @@
-print("Lux Finder started")
+from .base import BaseParser
+
+class DummyParser(BaseParser):
+    def __init__(self):
+        super().__init__("Dummy")
+
+    def run(self):
+        html = self.fetch("https://example.com")
+        print(f"Получено {len(html)} символов")
