83e14e7816c9866dcd41f68af4b6b42d680a7824
Kkkazhai <sergeykazhaev7@gmail.com>
2026-08-05 23:49:00 +0300

    Update dummy.py

--- parsers/dummy.py
+++ parsers/dummy.py
@@ -1,9 +1,1 @@
-from parsers.base import BaseParser
 
-class DummyParser(BaseParser):
-    def __init__(self):
-        super().__init__("Dummy")
-
-    def run(self):
-        html = self.fetch("https://example.com")
-        print(f"Получено {len(html)} символов")
