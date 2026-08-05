96c70e318a57fa364f532918a2f53ed2f5f61e00
Kkkazhai <sergeykazhaev7@gmail.com>
2026-08-05 14:02:46 +0300

    Create dummy.py

---
+++ parsers/dummy.py
@@ -0,0 +1,9 @@
+from .base import BaseParser
+
+class DummyParser(BaseParser):
+    def __init__(self):
+        super().__init__("Dummy")
+
+    def run(self):
+        html = self.fetch("https://example.com")
+        print(f"Получено {len(html)} символов")
