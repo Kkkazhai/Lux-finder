1edabbc6651502bdea665f0eae7411eac20afb31
Kkkazhai <sergeykazhaev7@gmail.com>
2026-08-06 20:01:21 +0300

    Update lukse.py

--- parsers/lukse.py
+++ parsers/lukse.py
@@ -7,11 +7,13 @@ class LukseParser(BaseParser):
     name = "LUKSE"
 
     def parse(self):
-        url = "https://lukse.ru/"
+        url = "https://lukse.ru/catalog/"
         html = self.get(url)
 
-        print(f"Получено {len(html)} символов")
+        with open("lukse.html", "w", encoding="utf-8") as f:
+            f.write(html)
 
         soup = BeautifulSoup(html, "lxml")
 
-        print(soup.title.string if soup.title else "Без заголовка")
+        print("HTML сохранен")
+        print(soup.title.string if soup.title else "Нет title")
